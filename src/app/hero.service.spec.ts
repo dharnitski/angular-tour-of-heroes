5dfa4f13e4d28e5ea1871ab1680e0ab3cadc200e
Dmitry Harnitski <dharnitski@gmail.com>
2019-04-13 18:58:37 -0400

    test HeroService.getHeroes

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,17 +1,61 @@
-import { HttpClient } from "@angular/common/http";
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-import { HeroService } from "./hero.service";
-import { MessageService } from "./message.service";
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { Hero } from './hero';
+
+const mockData = [
+  { id: 1, name: 'Hulk' },
+  { id: 2, name: 'Thor' },
+  { id: 3, name: 'Iron Man' }
+] as Hero[];
 
 describe('HeroService', () => {
-  let heroService;
-  let messageService: MessageService;
-  let httpClient: HttpClient;
-  beforeEach(() => { heroService = new HeroService(httpClient, messageService); });
 
+  let heroService;
+  let httpTestingController: HttpTestingController;
+  let mockHeroes;
+  let mockHero;
+  let mockId;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [HeroService, MessageService]
+    });
+
+    httpTestingController = TestBed.get(HttpTestingController);
+    heroService = TestBed.get(HeroService);
+
+    mockHeroes = [...mockData];
+    mockHero = mockHeroes[0];
+    mockId = mockHero.id;
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
   it('should exist', () => {
     expect(heroService).toBeTruthy();
+  });
+
+  describe('getHeroes', () => {
+
+    it('should return mock heroes', () => {
+      heroService.getHeroes().subscribe(
+        heroes => expect(heroes.length).toEqual(mockHeroes.length),
+        fail
+      );
+      // Receive GET request
+      const req = httpTestingController.expectOne(heroService.heroesUrl);
+      expect(req.request.method).toEqual('GET');
+      // Respond with the mock heroes
+      req.flush(mockHeroes);
+    });
   });
 });
 
